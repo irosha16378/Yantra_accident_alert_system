3b30a70b43fbdb4f5a858a22e6a798395329a0bc
Irosha Abeyrathna <irosha16378@gmail.com>
2026-09-15 10:41:16 +0530

    feat(cellular): add base header structure and hardware pin definitions

--- firmware/include/CellularManager.h
+++ firmware/include/CellularManager.h
@@ -2,12 +2,34 @@
 #define CELLULAR_MANAGER_H
 
 #include <Arduino.h>
+#include <HardwareSerial.h>
+#include "Config.h"
 
+/**
+ * @file CellularManager.h
+ * @brief 4G/LTE Cellular Communications Driver for Yantra Accident Alert System
+ */
 class CellularManager {
+private:
+    HardwareSerial& _serial;
+    int _rxPin;
+    int _txPin;
+    int _pwrPin;
+    uint32_t _baudRate;
+    bool _initialized;
+
 public:
-    CellularManager();
-    bool begin();
+    explicit CellularManager(HardwareSerial& serial = Serial1);
+
+    /**
+     * @brief Initialize cellular hardware UART and modem pins
+     */
+    bool begin(int rxPin = Config::PIN_MODEM_RX, 
+               int txPin = Config::PIN_MODEM_TX, 
+               int pwrPin = Config::PIN_MODEM_PWR, 
+               uint32_t baudRate = Config::MODEM_BAUD);
+
+    bool isInitialized() const { return _initialized; }
 };
 
 #endif // CELLULAR_MANAGER_H
-
--- firmware/src/CellularManager.cpp
+++ firmware/src/CellularManager.cpp
@@ -1,8 +1,18 @@
 #include "CellularManager.h"
 
-CellularManager::CellularManager() {}
+CellularManager::CellularManager(HardwareSerial& serial)
+    : _serial(serial),
+      _rxPin(Config::PIN_MODEM_RX),
+      _txPin(Config::PIN_MODEM_TX),
+      _pwrPin(Config::PIN_MODEM_PWR),
+      _baudRate(Config::MODEM_BAUD),
+      _initialized(false) {}
 
-bool CellularManager::begin() {
+bool CellularManager::begin(int rxPin, int txPin, int pwrPin, uint32_t baudRate) {
+    _rxPin = rxPin;
+    _txPin = txPin;
+    _pwrPin = pwrPin;
+    _baudRate = baudRate;
+    _initialized = true;
     return true;
 }
-
