452fc0e08fa77a35850518904efc4ad2455caaa2
Irosha Abeyrathna <irosha16378@gmail.com>
2026-09-17 21:48:04 +0530

    "feat(handlebar): implement grip state detection algorithm and hand presence evaluation"

--- firmware/src/HandlebarSensor.cpp
+++ firmware/src/HandlebarSensor.cpp
@@ -118,11 +118,50 @@ void HandlebarSensor::applyFilter() {
 }
 
 void HandlebarSensor::evaluateGripState() {
+    if (!_initialized) return;
+
+    _data.bothTouched = (_data.leftTouched && _data.rightTouched);
+    _data.handsOnBar  = (_data.leftTouched || _data.rightTouched);
+
+    GripState newState;
+    if (_data.leftTouched && _data.rightTouched) {
+        newState = GripState::BOTH_HANDS;
+    } else if (_data.leftTouched) {
+        newState = GripState::LEFT_ONLY;
+    } else if (_data.rightTouched) {
+        newState = GripState::RIGHT_ONLY;
+    } else {
+        newState = GripState::NO_HANDS;
+    }
+
+    uint32_t now = millis();
+
+    if (newState != _data.state) {
+        Serial.printf("[HANDLEBAR] Grip State Transition: %s -> %s (Left: %s, Right: %s)\n",
+                      getStateString(),
+                      (newState == GripState::BOTH_HANDS ? "BOTH_HANDS" :
+                       newState == GripState::LEFT_ONLY  ? "LEFT_ONLY" :
+                       newState == GripState::RIGHT_ONLY ? "RIGHT_ONLY" : "NO_HANDS"),
+                      _data.leftTouched ? "TOUCHED" : "RELEASED",
+                      _data.rightTouched ? "TOUCHED" : "RELEASED");
+
+        _data.state = newState;
+        _lastStateChangeTime = now;
+        _data.touchDurationMs = 0;
+        _data.handsOffDurationMs = 0;
+    } else {
+        if (_data.handsOnBar) {
+            _data.touchDurationMs = now - _lastStateChangeTime;
+            _data.handsOffDurationMs = 0;
+        } else {
+            _data.handsOffDurationMs = now - _lastStateChangeTime;
+            _data.touchDurationMs = 0;
+        }
+    }
 }
 
 bool HandlebarSensor::isHandsOffAlert(uint32_t timeoutMs) const {
-    (void)timeoutMs;
-    return false;
+    return (!_data.handsOnBar && (_data.handsOffDurationMs >= timeoutMs));
 }
 
 void HandlebarSensor::printStatus() const {
